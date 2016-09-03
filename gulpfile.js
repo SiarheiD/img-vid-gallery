70fcdaa6a027f2b9eceaf7083925306f2f2f7f9b
Siarhei Drahichynski <sd@imaguru.by>
2016-09-03 10:08:51 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp       = require('gulp'),
+	less         = require('gulp-less'),
+	browserSync  = require('browser-sync'),
+	concat       = require('gulp-concat'),
+	uglify       = require('gulp-uglifyjs'),
+	cssnano      = require('gulp-cssnano'),
+	rename       = require('gulp-rename'),
+	del          = require('del'),
+	imagemin     = require('gulp-imagemin'),
+	pngquant     = require('imagemin-pngquant'),
+	cache        = require('gulp-cache'),
+	autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('less', function(){
+	return gulp.src('app/less/**/*.less')
+	.pipe(less())
+	.pipe(autoprefixer(['last 15 versions']), {cascade: true})
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+	])
+	.pipe(concat('libs.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-libs', ['less'], function(){
+	return gulp.src([
+		'app/css/libs.css'
+	])
+	.pipe(cssnano())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('img', function(){
+	return gulp.src('app/img/**/*')
+	.pipe(cache(imagemin({
+		interlaced: true,
+		progressive: true,
+		svgoPlugins: [{removeViewBox: false}],
+		use: [pngquant()],
+	})))
+	.pipe(gulp.dest('dist/img/'))
+});
+
+gulp.task('clearCache', function(){
+	return cache.clearAll();
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: 'app',
+		},
+		notify: false,
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function(){
+	gulp.watch('app/less/**/*.less', ['less']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('clean', function(){
+	return del.sync('dist');
+});
+
+gulp.task('build', ['clean', 'less', 'scripts', 'img'], function() {
+	var buildCss = gulp.src([
+			'app/css/**/*	',
+		])
+	.pipe(gulp.dest('dist/css'));
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*')
+		.pipe(gulp.dest('dist/js'));
+
+	var buildHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
+});
